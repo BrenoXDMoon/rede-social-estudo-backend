70acd635f3f2670e751e2a080dac488f1370dd80
BrenoXDMoon <brenogabriel19@gmail.com>
2022-12-26 23:49:14 -0300

    Upload inicial

---
+++ src/main/java/com/example/redesocialestudobackend/RedeSocialEstudoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.redesocialestudobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialEstudoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialEstudoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redesocialestudobackend/RedeSocialEstudoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redesocialestudobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialEstudoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
